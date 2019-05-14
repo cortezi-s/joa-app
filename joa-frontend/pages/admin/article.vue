d15c0b5e75577732e8d5ca7a14505745daf45329
Rodrigo Villardi Cortezi <rodrigovcortezi@gmail.com>
2019-05-14 16:35:57 -0300

    Loading no botão após de requisição em todos os resources.

--- joa-frontend/pages/admin/article.vue
+++ joa-frontend/pages/admin/article.vue
@@ -3,17 +3,33 @@
     <p class="title is-size-2">Artigos</p>
     <form @submit.prevent="addArticle">
       <b-field label="Título">
-        <b-input v-model="newArticle.title"></b-input>
+        <b-input v-model="newArticle.title" :disabled="successSubmit">
+        </b-input>
       </b-field>
       <b-field label="Subtítulo">
-        <b-input v-model="newArticle.subtitle"></b-input>
+        <b-input v-model="newArticle.subtitle" :disabled="successSubmit">
+        </b-input>
       </b-field>
       <VueTrix v-model="newArticle.content" />
       <b-field></b-field>
       <b-field>
         <p class="control">
-          <button type="submit" class="button is-primary">
-            Salvar
+          <button
+            type="submit"
+            class="button"
+            :class="{
+              'is-primary': !successSubmit,
+              'is-success': successSubmit,
+              'is-loading': isSubmitting
+            }"
+            :disabled="successSubmit"
+          >
+            <span v-if="successSubmit" class="icon is-small">
+              <i class="fas fa-check"></i>
+            </span>
+            <span>
+              {{ submitText }}
+            </span>
           </button>
         </p>
       </b-field>
@@ -31,14 +47,23 @@ export default {
   },
   data() {
     return {
+      isSubmitting: false,
+      successSubmit: false,
+      submitText: 'Salvar',
       newArticle: {}
     }
   },
   methods: {
     async addArticle() {
+      this.isSubmitting = true
       const result = await this.$axios.$post('articles', {
         article: this.newArticle
       })
+      const trixEditor = document.querySelector('trix-editor')
+      trixEditor.setAttribute('contentEditable', 'false')
+      this.isSubmitting = false
+      this.successSubmit = true
+      this.submitText = 'Salvo'
       console.log(result)
     }
   }
--- joa-frontend/pages/admin/project.vue
+++ joa-frontend/pages/admin/project.vue
@@ -3,46 +3,77 @@
     <p class="title is-size-2">Projetos</p>
     <form @submit.prevent="addProject">
       <b-field label="Nome da empresa">
-        <b-input v-model="newProject.company_name"></b-input>
+        <b-input v-model="newProject.company_name" :disabled="successSubmit">
+        </b-input>
       </b-field>
       <b-field label="Fundadores">
-        <b-input v-model="newProject.founders"></b-input>
+        <b-input v-model="newProject.founders" :disabled="successSubmit">
+        </b-input>
       </b-field>
       <b-field grouped>
         <b-field label="Localização" expanded>
-          <b-input v-model="newProject.location"></b-input>
+          <b-input v-model="newProject.location" :disabled="successSubmit">
+          </b-input>
         </b-field>
         <b-field label="Data" expanded>
-          <b-datepicker placeholder="Selecione uma data..." icon="calendar-alt">
+          <b-datepicker
+            placeholder="Selecione uma data..."
+            icon="calendar-alt"
+            :disabled="successSubmit"
+          >
           </b-datepicker>
         </b-field>
       </b-field>
       <b-field label="Descrição">
-        <b-input v-model="newProject.description" type="textarea"></b-input>
+        <b-input
+          v-model="newProject.description"
+          type="textarea"
+          :disabled="successSubmit"
+        >
+        </b-input>
       </b-field>
       <b-field label="Página web">
-        <b-input v-model="newProject.homepage"></b-input>
+        <b-input v-model="newProject.homepage" :disabled="successSubmit">
+        </b-input>
       </b-field>
       <b-field grouped>
         <b-field label="Facebook" expanded>
-          <b-input v-model="newProject.facebook"></b-input>
+          <b-input v-model="newProject.facebook" :disabled="successSubmit">
+          </b-input>
         </b-field>
         <b-field label="Linkedin" expanded>
-          <b-input v-model="newProject.linkedin"></b-input>
+          <b-input v-model="newProject.linkedin" :disabled="successSubmit">
+          </b-input>
         </b-field>
       </b-field>
       <b-field grouped>
         <b-field label="Twitter" expanded>
-          <b-input v-model="newProject.twitter"></b-input>
+          <b-input v-model="newProject.twitter" :disabled="successSubmit">
+          </b-input>
         </b-field>
         <b-field label="Instagram" expanded>
-          <b-input v-model="newProject.instagram"></b-input>
+          <b-input v-model="newProject.instagram" :disabled="successSubmit">
+          </b-input>
         </b-field>
       </b-field>
       <b-field>
         <p class="control">
-          <button type="submit" class="button is-primary">
-            Salvar
+          <button
+            type="submit"
+            class="button"
+            :class="{
+              'is-primary': !successSubmit,
+              'is-success': successSubmit,
+              'is-loading': isSubmitting
+            }"
+            :disabled="successSubmit"
+          >
+            <span v-if="successSubmit" class="icon is-small">
+              <i class="fas fa-check"></i>
+            </span>
+            <span>
+              {{ submitText }}
+            </span>
           </button>
         </p>
       </b-field>
@@ -55,14 +86,21 @@ export default {
   name: 'ProjectAdmin',
   data() {
     return {
+      isSubmitting: false,
+      successSubmit: false,
+      submitText: 'Salvar',
       newProject: {}
     }
   },
   methods: {
     async addProject() {
+      this.isSubmitting = true
       const result = await this.$axios.$post('projects', {
         project: this.newProject
       })
+      this.isSubmitting = false
+      this.successSubmit = true
+      this.submitText = 'Salvo'
       console.log(result)
     }
   }
