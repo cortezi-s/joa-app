e970bf8f9c5b638606a02560a4b299a0c4c5e75d
Rodrigo Villardi Cortezi <rodrigovcortezi@gmail.com>
2019-07-09 19:34:29 -0300

    Criação de rotas para controllers admin.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
 	mount_devise_token_auth_for 'User', at: 'auth'
 	namespace :api do
 		namespace :v1 do
+			namespace :admin do
+				resources :articles
+				resources :projects
+				resources :collaborators
+			end
 		  resources :articles
 		  resources :projects
 		  resources :collaborators
