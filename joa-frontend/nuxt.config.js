da5725f2e44950dd36d24b4e96f5b928ee3da391
tcortezi <tvcortezi@gmail.com>
2019-07-10 02:13:49 -0300

    Altera meta description

--- joa-frontend/nuxt.config.js
+++ joa-frontend/nuxt.config.js
@@ -19,7 +19,7 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { hid: 'description', name: 'description', content: 'Buscamos negócios inovadores visando a geração de valor ao longo prazo' }
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/icon.ico' }]
   },
