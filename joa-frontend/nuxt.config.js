c6c6631d16facdad478d25b0a7d44ea50618b832
tcortezi <tvcortezi@gmail.com>
2019-08-22 20:04:32 -0300

    Altera proxy para hostname com https

--- joa-frontend/nuxt.config.js
+++ joa-frontend/nuxt.config.js
@@ -82,7 +82,7 @@ module.exports = {
     '/api/v1/': 'http://localhost:3000',
     '/auth/': 'http://localhost:3000',
     '/contact': 'http://localhost:3000',
-    '/rails/active_storage/': 'http://23.92.20.101:3000'
+    '/rails/active_storage/': 'https://joacapital.com.br:3000'
   },
 
   /*
